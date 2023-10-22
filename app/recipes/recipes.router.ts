cc0c4c0f3a5817490e54b5ce61e31bdbf7f0adb1
camchu2001 <chuphuongcam@gmail.com>
2023-10-22 13:01:50 -0500

    feat: add getRecipes service function

--- app/recipes/recipes.router.ts
+++ app/recipes/recipes.router.ts
@@ -1,8 +1,11 @@
 import express from 'express';
-import { createRecipeHandler, getRecipeHandler } from './recipes.ctrl';
+import {
+    createRecipeHandler, getRecipeHandler, getRecipesHandler
+} from './recipes.ctrl';
 import { requestValidator } from '../../middleware';
 
 export const recipeRouter = express.Router();
 
 recipeRouter.get( '/:recipeId', requestValidator( 'GET_RECIPE' ), getRecipeHandler );
+recipeRouter.get( '/', requestValidator( 'GET_RECIPES' ), getRecipesHandler );
 recipeRouter.post( '/', requestValidator( 'CREATE_RECIPE' ), createRecipeHandler );
--- app/recipes/recipes.validation.ts
+++ app/recipes/recipes.validation.ts
@@ -2,6 +2,12 @@ import Joi from 'joi';
 
 export const GET_RECIPE = Joi.object( { params: Joi.object( { recipeId: Joi.number() } ) } );
 
+export const GET_RECIPES = Joi.object( {
+    query: Joi.object( { userId: Joi.string() } )
+        .required()
+        .options( { presence: 'required' } )
+} );
+
 export const CREATE_RECIPE = Joi.object( {
     body: Joi.object(
         {
