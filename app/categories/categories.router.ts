c28f8ef1eb9d1d4530fb0659b0f84b66805c9e28
camchu2001 <chuphuongcam@gmail.com>
2023-11-09 20:48:17 -0600

    chore: add categories joi validation

--- app/categories/categories.router.ts
+++ app/categories/categories.router.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import { createCategoryHandler, getCategoryHandler } from './categories.controller';
+import { requestValidator } from '../../middleware';
 
 export const categoryRouter = express.Router();
 
-categoryRouter.get( '/:categoryId', getCategoryHandler );
-categoryRouter.post( '/', createCategoryHandler );
+categoryRouter.get( '/:categoryId', requestValidator( 'GET_CATEGORY' ), getCategoryHandler );
+categoryRouter.post( '/', requestValidator( 'CREATE_CATEGORY' ), createCategoryHandler );
---
+++ app/categories/categories.validation.ts
@@ -0,0 +1,13 @@
+import Joi from 'joi';
+
+export const GET_CATEGORY = Joi.object( { params: Joi.object( { categoryId: Joi.number() } ) } );
+
+export const CREATE_CATEGORY = Joi.object( {
+    body: Joi.object(
+        {
+            name: Joi.string(),
+            description: Joi.string()
+        }
+    ).options( { presence: 'required' } )
+        .required()
+} );
